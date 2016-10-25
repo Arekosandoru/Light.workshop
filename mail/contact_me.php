b83c56dc8fd5cc4540f0f1aa1208f041cff04899
Arekosandoru <borom1993>
2016-10-25 16:20:03 +0700

    1

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,11 +11,11 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
    
 // Create the email and send the message
-$to = 'oboromen@yandex.ru'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'masterskaya_svet@mail.ru'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n;
 $headers = "From: noreply@lightworkshop.ru\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $phone";   
+$headers .= "Reply-To: $phone,' ', $name";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
