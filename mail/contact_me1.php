3ef4bdde5b0636ce8ba46e932b5e4a1bd0227102
Aleksandr Bendyukov <oboromen@yandex.ru>
2016-11-21 10:30:13 +0700

    2

--- mail/contact_me1.php
+++ mail/contact_me1.php
@@ -9,10 +9,10 @@
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
    
-$to = 'masterskaya_svet@mail.ru';
+$to = 'arekosandor@yandex.ru';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n";
-$headers = "From: noreply@lightworkshop.ru\n"; 
+$headers = "From: 1@мастерскаясвет.рф\n"; 
 $headers .= "Reply-To: $phone,' ', $name";   
 mail($to,$email_subject,$email_body,$headers);
 // return true;         
